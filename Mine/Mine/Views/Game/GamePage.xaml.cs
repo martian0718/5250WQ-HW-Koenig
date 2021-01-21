8e476fa5bedb62a58b9e9249e3ff8bd5cec68d5e
martian0718 <martian0718@gmail.com>
2021-01-20 20:30:35 -0800

    Added Game Page, Updated HomeMenuItem to have Game, updated MainPage and Menu Page to include the GamePage.  Change to HomeMenuItem.cs, GamePage.xaml, GamePage.xaml.cs, MainPage.xaml.cs, Menupage.xaml.cs and Mine.csproj

---
+++ Mine/Mine/Views/Game/GamePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Mine.Views
+{
+    public partial class GamePage : ContentPage
+    {
+        public GamePage()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
