9a2ad73b9e87cc75cbb44f1dabc13463f5e6d97e
martian0718 <martian0718@gmail.com>
2021-01-28 14:39:31 -0800

    Renamed ItemDetailPage to ItemReadPage, changed ItemReadPage.xaml, ItemReadPage.xaml.cs, ItemsPage.xaml.cs, Mine.csproj

--- Mine/Mine/Views/Items/ItemsPage.xaml.cs
+++ Mine/Mine/Views/Items/ItemsPage.xaml.cs
@@ -38,7 +38,7 @@ namespace Mine.Views
             if (item == null)
                 return;
 
-            await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
+            await Navigation.PushAsync(new ItemReadPage(new ItemDetailViewModel(item)));
 
             // Manually deselect item.
             ItemsListView.SelectedItem = null;
