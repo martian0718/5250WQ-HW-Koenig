1f3ca8206d2999cb950d7285163604b9ef084968
martian0718 <martian0718@gmail.com>
2021-02-01 21:04:53 -0800

    Added ReadAsync implementation to DatabaseService.cs

--- Mine/Mine/Services/DatabaseService.cs
+++ Mine/Mine/Services/DatabaseService.cs
@@ -66,7 +66,15 @@ namespace Mine.Services
 
         public Task<ItemModel> ReadAsync(string id)
         {
-            throw new NotImplementedException();
+            if(id == null)
+            {
+                return null;
+            }
+
+            //Call the Database to read the ID
+            // Using Linq syntax to find the first record that has the ID that matches
+            var result = Database.Table<ItemModel>().FirstOrDefaultAsync(m => m.Id.Equals(id));
+            return result;
         }
 
         /// <summary>
