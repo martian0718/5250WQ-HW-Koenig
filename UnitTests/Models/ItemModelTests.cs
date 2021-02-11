41b9d994ea871b1ebe0d0dba431d2a624283d7ee
martian0718 <martian0718@gmail.com>
2021-02-10 20:30:18 -0800

    Added ItemModelTests.cs to the proejct

---
+++ UnitTests/Models/ItemModelTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class ItemModelTests
+    {
+        [Test]
+        public void ItemModel_Constructor_Valid_()
+        {
+            //Arrange
+            //Act
+            //Reset
+            //Assert
+            Assert.Pass();
+        }
+    }
+}
